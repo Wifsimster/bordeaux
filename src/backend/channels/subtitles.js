fe0bb0c9319fa983ddd6975d1afe432604990fd1
Damien BATTISTELLA <damien.battistella@agfa.com>
2019-09-11 16:35:27 +0200

    Add languages options to getSubtitle()

--- src/backend/channels/subtitles.js
+++ src/backend/channels/subtitles.js
@@ -8,28 +8,34 @@ class Subtitles {
   constructor() {}
 
   static async response(data) {
+    const subtitlesSettings = File.readFile(SUBTITLES_FILE)
+    const directorySettings = File.readFile(DIRECTORY_FILE)
+
     switch (data.method) {
       case "getAll":
-        data.results = File.readFile(SUBTITLES_FILE)
+        data.results = subtitlesSettings
         break
       case "update":
         File.writeFile(SUBTITLES_FILE, data.params)
         data.results = File.readFile(SUBTITLES_FILE)
         break
       case "search":
-        var settings = File.readFile(DIRECTORY_FILE)
         data.results = Yquem.getRecentFilesFromDirectory(
-          settings.to,
-          data.params.fileAge
+          directorySettings.to,
+          subtitlesSettings.daysOld
         )
         break
       case "getSubtitle":
-        data.results = await Yquem.getSubtitle(data.params.file).catch(err => {
+        data.results = await Yquem.getSubtitle(data.params.file, {
+          languages: subtitlesSettings.languages
+        }).catch(err => {
           data.error = err
         })
         break
       case "hasSubtitle":
-        data.results = Yquem.hasSubtitle(data.params.file)
+        data.results = Yquem.hasSubtitle(data.params.file, {
+          languages: subtitlesSettings.languages
+        })
         break
       default:
         console.log(`[Subtitles] Unknow method : ${data.method}`)
