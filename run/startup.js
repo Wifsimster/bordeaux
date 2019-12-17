0a6da6856ab819518e402758f4d7928ee07ba76a
Damien BATTISTELLA <damien.battistella@agfa.com>
2019-12-17 14:28:16 +0100

    Fix script

--- run/startup.js
+++ run/startup.js
@@ -1,19 +1,26 @@
 const { exec } = require("child_process")
+const path = require("path")
 
-exec(`node "${__dirname}\\back.server.js"`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Couldn't execute the command : 'back.server.js'`, err)
-    return
+exec(
+  `node "${path.join(__dirname, "back.server.js")}"`,
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Couldn't execute the command : 'back.server.js'`, err)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
   }
-  console.log(`stdout: ${stdout}`)
-  console.log(`stderr: ${stderr}`)
-})
+)
 
-exec(`node "${__dirname}\\dev.server.js"`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Couldn't execute the command : 'dev.server.js'`, err)
-    return
+exec(
+  `node "${path.join(__dirname, "dev.server.js")}"`,
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Couldn't execute the command : 'dev.server.js'`, err)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
   }
-  console.log(`stdout: ${stdout}`)
-  console.log(`stderr: ${stderr}`)
-})
+)
